47c7e7eaa49ece0f67d2be9388f4346a15537bf3
Enrique <Enrique@ASUS_BOOK>
2025-05-26 15:16:39 -0500

    avance

--- Problemas-Java/src/main/java/dao/listaEnlazada.java
+++ Problemas-Java/src/main/java/dao/listaEnlazada.java
@@ -1,13 +1,87 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
+
+import dto.Nodo;
+import dto.Producto;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Enrique
  */
 public class listaEnlazada {
+    private Nodo cabeza;
+
+    public listaEnlazada() {
+        cabeza = null;
+    }
     
+    public void insertarFinal(Producto producto){
+        Nodo nuevoNodo = new Nodo(producto);
+        if(cabeza == null){
+            cabeza = nuevoNodo;
+        } else {
+            Nodo actual = cabeza;
+            while(actual.getSiguiente() != null){
+                actual = actual.getAnterior();
+            } 
+            actual.setSiguiente(nuevoNodo);
+        }
+    }
+    
+    //Metodo para eliminar el ultimo nodo
+    public void eliminarFinal(){
+        if(cabeza == null){
+            JOptionPane.showMessageDialog(null, "La lista esta Vacia, no hay nada que eliminar.");
+        } else if(cabeza.getSiguiente() == null) {
+            cabeza = null;
+            JOptionPane.showMessageDialog(null, "La lista se ha vaciado.");
+        } else {
+            Nodo actual = cabeza;
+            while(actual.getSiguiente().getSiguiente() != null){
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(null);
+            JOptionPane.showMessageDialog(null, "Se elimino el ultimo elemento.");
+        }
+    }
+    
+    //Metodo para mostrar la lista
+    public void mostrarEnlazadaAtras(){
+        if(cabeza == null){
+            JOptionPane.showMessageDialog(null, "La lista esta Vacia, no hay nada que mostrar.");
+            return;
+        } 
+        Nodo actual = cabeza;
+        while(actual.getSiguiente() != null){
+            actual = actual.getSiguiente();
+        }
+        String mensaje = "Lista Enlazada - Producto\n";
+        while(actual != null){
+            Producto a = new Producto();
+            mensaje += a.getCodiProd() + " - " + a.getNombProd() + " - " + a.getPrecProd() + " - " + a.getCantProd() + " - " + a.getCostoProd() + "\n";
+            actual = actual.getAnterior();
+        }
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+    
+    public double calcProm(){
+        double prom = 0;
+        if(cabeza == null){
+            //JOptionPane.showMessageDialog(null, "No hay productos, promedio vacio.");
+            return 0;
+        } 
+        Nodo actual = cabeza;
+        int tam = 0;
+        while(actual != null){
+            tam++;
+            actual = actual.getSiguiente();
+        }
+        actual = cabeza;
+        while(actual != null){
+            Producto a = new Producto();
+            prom += (a.getCantProd() + a.getCostoProd())/tam;
+            actual = actual.getSiguiente();
+        }
+        return prom;
+    }
 }
--- Problemas-Java/src/main/java/dto/Nodo.java
+++ Problemas-Java/src/main/java/dto/Nodo.java
@@ -9,5 +9,40 @@ package dto;
  * @author Enrique
  */
 public class Nodo {
+    private Producto producto;
+    private Nodo siguiente;
+    private Nodo anterior;
+
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+
+    public Nodo() {
+    }
+
+    public Nodo(Producto producto) {
+        this.producto = producto;
+        this.siguiente = null;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
     
 }
