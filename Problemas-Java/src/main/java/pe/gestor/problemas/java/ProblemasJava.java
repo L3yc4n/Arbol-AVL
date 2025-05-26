b00e70b4a425086877138ffbd47591772d7b02d3
Enrique <Enrique@ASUS_BOOK>
2025-05-26 13:03:23 -0500

    pila

---
+++ Problemas-Java/src/main/java/pe/gestor/problemas/java/ProblemasJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.gestor.problemas.java;
+
+/**
+ *
+ * @author Enrique
+ */
+public class ProblemasJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
